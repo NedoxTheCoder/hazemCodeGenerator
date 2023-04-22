3b51ea25f289abcca1b1bc67fc6313cb603514d3
Nedim Çoban <108099926+NedoxTheCoder@users.noreply.github.com>
2023-04-22 23:39:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+//* Declare
+
+
+
+
+const generate = document.querySelector(".generate");
+const shown = document.querySelector(".willbeshown")
+const copyButton = document.querySelector(".copy")
+
+
+
+
+
+
+copyButton.addEventListener('click', function(){
+    // getting the text content that we want to copy
+    const content = shown.textContent;
+    navigator.clipboard.writeText(content);
+    navigator.clipboard.readText()
+    .then((copied) => {
+        console.log(copied);
+    });
+})
+  // Alert the copied text
+
+
+
+const nums_lett_arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+let pass = ""
+
+function passHazemn(){
+    for (let i = 0; i < 7 ; i++){
+      const randomRate = Math.floor(Math.random()*62)
+      pass += nums_lett_arr[randomRate]
+    }
+    shown.innerText = `PL$${pass}$`
+    pass = ""
+  }
+
+
+
+generate.addEventListener("click",function(e){
+    e.preventDefault();
+    passHazemn();
+    // console.log(output1.innerHTML);
+    
+});
+
